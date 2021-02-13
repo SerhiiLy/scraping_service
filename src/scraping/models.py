a338e49d2169723f0d962b784180a0660098de2c
serhii lypak <lypak.serhii@gmail.com>
2021-02-13 15:16:43 +0200

    add templates(base, home) for scrapping, models(Vacancy)

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -29,3 +29,16 @@ class Language(models.Model):
         if not self.slug:
             self.slug = from_cyrillic_to_eng(str(self.name))
         super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250)
+    company = models.CharField(max_length=250)
+    description = models.TextField()
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
